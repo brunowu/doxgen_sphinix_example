08b8a55e3dc7253b3ba95bd714d24388b40b641d
Xinzhe Wu <xin.wu@fz-juelich.de>
2021-02-17 22:18:49 +0100

    rm build

--- build/docs/CMakeFiles/Sphinx.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../sphinx/html/index.html"
-  "CMakeFiles/Sphinx"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Sphinx.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
